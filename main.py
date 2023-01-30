b87139a09fd9a8206fd87710e0ae11a8ae418d80
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-01-30 20:39:19 +0100

    Setup simple get and post methods

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+# to run live server type in the terminal:
+# $ uvicorn main:app --reload
+
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello Dude!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+@app.post("/")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title: {payLoad['title']}; content: {payLoad['content']}"}
