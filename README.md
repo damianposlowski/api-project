f9cc0728d35f6bac4c630450a7172bf119647146
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-02-01 15:26:58 +0100

    Add data validation using pydantic

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is a project to develop an API from scratch using Python, FastAPI and PostgreSQL. The project is written and executed on a virtual environment, using Python version 3.10.6
 
-To test the API functions I use Postman.
+To test the API functions I used Postman. To validate the data schema I used pydantic.
--- main.py
+++ main.py
@@ -3,8 +3,16 @@
 
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -15,6 +23,8 @@ def get_posts():
     return {"data": "This is your post"}
 
 @app.post("/")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title: {payLoad['title']}; content: {payLoad['content']}"}
+def create_posts(post: Post):
+    print(post.rating)
+    print(post)
+    print(post.dict())
+    return {"data": post}
