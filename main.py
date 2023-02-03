15a1ac87fd11ec3bbd6a7c851bf46a807a16e146
Damian <39247696+damianposlowski@users.noreply.github.com>
2023-02-03 14:02:44 +0100

    Add status codes, for create (201) and search (404)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # to run live server type in the terminal:
 # $ uvicorn main:app --reload
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -34,7 +34,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 1000000)
@@ -42,7 +42,9 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    post = list(filter(lambda item: item["id"] == int(id), my_posts))[0]
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
     return {"post detail": post}
-    # return find_post(id)
